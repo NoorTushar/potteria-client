26a96af5128e85ebff405eb2ef8713656b017bab
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-26 14:32:33 +0600

    dark mode toggle working

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 
 const navItems = (
@@ -13,6 +14,28 @@ const navItems = (
 );
 
 const Navbar = () => {
+   // use theme from local storage if available or set light theme
+   const [theme, setTheme] = useState(
+      localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+   );
+
+   // update state on toggle
+   const handleToggle = (e) => {
+      if (e.target.checked) {
+         setTheme("luxury");
+      } else {
+         setTheme("light");
+      }
+   };
+
+   // set theme state in localstorage on mount & also update localstorage on state change
+   useEffect(() => {
+      localStorage.setItem("theme", theme);
+      const localTheme = localStorage.getItem("theme");
+      // add custom data-theme attribute to html tag required to update theme using DaisyUI
+      document.querySelector("html").setAttribute("data-theme", localTheme);
+   }, [theme]);
+
    return (
       <div className="navbar bg-base-100">
          <div className="navbar-start">
@@ -54,9 +77,51 @@ const Navbar = () => {
          <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{navItems}</ul>
          </div>
-         <div className="navbar-end">
+         <div className="navbar-end flex items-center">
             {/* theme toggler */}
-            <a className="btn">Button</a>
+            <button>
+               <label className="cursor-pointer grid place-items-center">
+                  <input
+                     className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+                     type="checkbox"
+                     onChange={handleToggle}
+                     // show toggle image based on localstorage theme
+                     checked={theme === "light" ? false : true}
+                  />
+                  <svg
+                     className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                     xmlns="http://www.w3.org/2000/svg"
+                     width="14"
+                     height="14"
+                     viewBox="0 0 24 24"
+                     fill="none"
+                     stroke="currentColor"
+                     strokeWidth="2"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                  >
+                     <circle cx="12" cy="12" r="5" />
+                     <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                  </svg>
+                  <svg
+                     className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                     xmlns="http://www.w3.org/2000/svg"
+                     width="14"
+                     height="14"
+                     viewBox="0 0 24 24"
+                     fill="none"
+                     stroke="currentColor"
+                     strokeWidth="2"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
+                  >
+                     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                  </svg>
+               </label>
+            </button>
+            <button className="btn">
+               <Link to={"/"}>Register</Link>
+            </button>
          </div>
       </div>
    );
