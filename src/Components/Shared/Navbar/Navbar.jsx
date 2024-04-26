c0f0157fad77e4a58520a2ccd3529e4ddc65d2e7
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-26 15:31:10 +0600

    navbar desing done so far, with login button added in last

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import "./Navbar.css";
 
 const navItems = (
    <>
-      <li className="my-nav-item text-brownPrimary">
+      <li className="my-nav-item text-brownPrimary tracking-[1.2px]">
          <NavLink to={"/"}>Home</NavLink>
       </li>
-      <li className="my-nav-item text-brownPrimary">
-         <NavLink to={"/about"}>About</NavLink>
+      <li className="my-nav-item text-brownPrimary tracking-[1.2px]">
+         <NavLink to={"/items"}>Items</NavLink>
       </li>
    </>
 );
@@ -75,11 +75,11 @@ const Navbar = () => {
             </Link>
          </div>
          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1 uppercase text-[12px] space-x-2">
+            <ul className="menu menu-horizontal px-1 uppercase text-[14px] space-x-2">
                {navItems}
             </ul>
          </div>
-         <div className="navbar-end flex items-center">
+         <div className="navbar-end flex items-center gap-2">
             {/* theme toggler */}
             <button>
                <label className="cursor-pointer grid place-items-center">
@@ -121,9 +121,16 @@ const Navbar = () => {
                   </svg>
                </label>
             </button>
-            <button className="btn">
-               <Link to={"/"}>Register</Link>
-            </button>
+
+            <Link
+               to={"/login"}
+               className="relative inline-flex items-center justify-start px-5 py-2 overflow-hidden font-medium transition-all bg-white  hover:bg-white group border-2 border-[#a65f3f] text-[14px] tracking-[1.2px]"
+            >
+               <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#a65f3f]  absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+               <span className="relative w-full text-left text-[#a65f3f] transition-colors duration-300 ease-in-out group-hover:text-white">
+                  LOGIN
+               </span>
+            </Link>
          </div>
       </div>
    );
