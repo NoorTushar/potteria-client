8c15f9f6e1a6092dc95a057d5f6ddaecea0cec6f
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-28 01:01:10 +0600

    my items page card design done. next will make update and delete button fuctional

--- src/Components/MyItemCard/MyItemCard.jsx
+++ src/Components/MyItemCard/MyItemCard.jsx
@@ -19,13 +19,13 @@ const MyItemCard = ({ item }) => {
       <div className="card bg-base-100 shadow-xl rounded-none">
          <figure>
             <img
-               className="hover:scale-110 object-cover duration-300"
+               className="hover:scale-110 object-cover duration-300 h-[320px] w-full"
                src={image}
                alt={item_name}
             />
          </figure>
          <div className="card-body">
-            <h2 className="card-title uppercase tracking-[3px] text-brownPrimary">
+            <h2 className="card-title uppercase tracking-[3px] text-brownPrimary  text-[22px] ">
                {item_name}
             </h2>
 
@@ -40,7 +40,7 @@ const MyItemCard = ({ item }) => {
                </p>
             </div>
 
-            <div className="lg:flex justify-between space-y-2 lg:space-y-0">
+            <div className="lg:flex justify-between space-y-2 lg:space-y-0   flex-grow">
                <p className="text-[#58595b] capitalize leading-[25px] font-light">
                   <span className="text-base font-semibold">
                      Customization:
@@ -52,7 +52,8 @@ const MyItemCard = ({ item }) => {
                </p>
             </div>
 
-            <div className="card-actions justify-end">
+            {/* Buttons */}
+            <div className="card-actions justify-between mt-1">
                <div className="mt-3 text-center">
                   <button className="relative inline-flex items-center justify-start px-5 py-2 overflow-hidden font-medium transition-all bg-brownPrimary  hover:bg-white group border-2 border-[#a65f3f] text-[13px] tracking-[1.2px]">
                      <span className="w-48 h-48 rounded rotate-[-40deg] bg-white  absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
